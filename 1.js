ef3fcb6a58f606d4b8d5f15720ae2565dc5729ee
Geeteshwer Vashishth <geeteshwers@gmail.com>
2026-08-25 11:21:07 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,6 @@
+let a = 3;
+let b = 9;
+console.log("SUM: ",a+b)
+console.log("Substract: ",a-b)
+console.log("Multiply: ",a*b)
+console.log("Divide: ",a/b)
---
+++ function.js
@@ -0,0 +1,7 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+let a = Number(prompt("Enter first number:"));
+let b = Number(prompt("Enter second number:"));
+document.write("Result = " + multiply(a, b));
